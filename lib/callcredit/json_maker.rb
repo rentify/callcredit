622f9953fbc43154eafbb5d593af276211eabdaa
Aleksandar Simic <asimic@gmail.com>
2013-07-05 15:49:38 +0100

    parsing of income_type for JSON

--- lib/callcredit/json_maker.rb
+++ lib/callcredit/json_maker.rb
@@ -9,13 +9,14 @@ class JSONmaker
     # demographic
     demographic = hash[:search07a_response][:search_result][:creditreport][:applicant][:demographics2006]
     financial_risk = demographic[:cameofing] || "not found"
-    income_type = demographic[:camoeincome]
+    income_type = demographic[:cameoincome]
     investor_category = demographic[:cameoinvestor]
     property_value = demographic[:cameoproperty]
     area_makeup = demographic[:cameouk]
 
     { forename: forename, surname: surname, dob: dob, addresses: addresses,
-      financial_risk: get_financial_risk(financial_risk) }
+      financial_risk: get_financial_risk(financial_risk),
+      income_type: get_income_type(income_type) }
   end
 
   private
@@ -31,5 +32,62 @@ class JSONmaker
     }
 
     cameofing[key]
+  end
+
+  def self.get_income_type key
+    # Income type
+    cameoincome = {
+      "11" => "Opulent Families & Couples",
+      "12" => "Professional Detached Homeowners",
+      "13" => "Retired Flat Dwellers",
+      "14" => "Affluent Mixed Neighbourhoods",
+      "15" => "Wealthy Terraced Households",
+      "16" => "Affluent Young Professionals",
+      "21" => "Affluent Families & Couples",
+      "22" => "Older Professional Families",
+      "23" => "Retired Professionals",
+      "24" => "Mixed Occupancy Flats",
+      "25" => "Home Owning Managers & Professionals",
+      "26" => "Educated Couples & Singles",
+      "31" => "Home Owning Families & Couples",
+      "32" => "Professional Detached Neighbourhoods",
+      "33" => "Accomplished Retirees",
+      "34" => "Well-Off Flat Tenants",
+      "35" => "Professional Terraced Residents",
+      "36" => "Young Managers & Professionals",
+      "41" => "Comfortable Families & Couples",
+      "42" => "Managers & Professionals In Detached Homes",
+      "43" => "Retired Homeowners",
+      "44" => "Aspiring Households In Mixed Neighbourhoods",
+      "45" => "Comfortable Terraced Neighbourhoods",
+      "46" => "Professional Singles & Cohabitees",
+      "51" => "Middle-Aged Families & Couples",
+      "52" => "Families & Couples In Detached Housing",
+      "53" => "Retired Flat Tenants",
+      "54" => "Flat Dwelling Families",
+      "55" => "Terraced Homeowners",
+      "56" => "Young Professional Flat Dwellers",
+      "61" => "Families In Semi-Detached Areas",
+      "62" => "Older Detached Neighbourhoods",
+      "63" => "Less Affluent Retired Households",
+      "64" => "Lower Income Tenants",
+      "65" => "Less Affluent Terraced Homeowners",
+      "66" => "Couples & Singles In Flats",
+      "71" => "Lower Skilled Families & Couples",
+      "72" => "Poorer Detached Homeowners",
+      "73" => "Retirees In Less Affluent Areas",
+      "74" => "Lower Class Families",
+      "75" => "Poorer Terraced Communities",
+      "76" => "Young Professionals In Student Areas",
+      "81" => "Lower Income Older Families",
+      "82" => "Poorer Owner Occupiers",
+      "83" => "Retired Council Flat Tenants",
+      "84" => "Lower Class Council Neighbourhoods",
+      "85" => "Lower Income Terraced Households",
+      "86" => "Couples & Singles In Rented Accommodation",
+      "XX" => "Communal Establishments In Mixed Neighbourhoods"
+    }
+    cameoincome[key]
+    # key
   end
 end
