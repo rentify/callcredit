e4e451276a0af3f65a0bbf03836422302419f3b5
Aleksandar Simic <asimic@gmail.com>
2013-07-05 16:21:26 +0100

    really removed address & person 'models'

--- lib/callcredit/address.rb
+++
@@ -1,9 +0,0 @@
-class Address
-  attr_accessor :number, :postcode
-  def initialize(*params)
-    unless params.count == 0
-      @number = params.first[:number]
-      @postcode = params.first[:postcode]
-    end
-  end
-end
