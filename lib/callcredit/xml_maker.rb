b5f6e4f252d9c97a44676d688f9bd1ab8a9f3f37
Ryan Harkins <silverphp@gmail.com>
2013-07-31 00:43:45 +0100

    Indent private methods properly, easier to read

--- lib/callcredit/xml_maker.rb
+++ lib/callcredit/xml_maker.rb
@@ -36,92 +36,92 @@ module CallCredit
 
 
     private
-    def self.fix_root_node(builder)
-      builder.doc.root.node_name = "soapenv:Envelope"
-      builder.doc.root.to_xml
-    end
+      def self.fix_root_node(builder)
+        builder.doc.root.node_name = "soapenv:Envelope"
+        builder.doc.root.to_xml
+      end
 
-    def self.generate_header(data)
-      ns = { "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
-        "xmlns:soap" => "urn:callcredit.co.uk/soap:callreport7" }
+      def self.generate_header(data)
+        ns = { "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
+          "xmlns:soap" => "urn:callcredit.co.uk/soap:callreport7" }
 
-      doc = Nokogiri::XML::Builder.new do |xml|
-        xml.root(ns) do
-          xml["soapenv"].Header do
-            xml["soap"].callcreditheaders do
-              xml["soap"].company  CallCredit.configuration.company
-              xml["soap"].username CallCredit.configuration.username
-              xml["soap"].password CallCredit.configuration.password
+        doc = Nokogiri::XML::Builder.new do |xml|
+          xml.root(ns) do
+            xml["soapenv"].Header do
+              xml["soap"].callcreditheaders do
+                xml["soap"].company  CallCredit.configuration.company
+                xml["soap"].username CallCredit.configuration.username
+                xml["soap"].password CallCredit.configuration.password
+              end
             end
+            xml["soapenv"].Body
           end
-          xml["soapenv"].Body
         end
       end
-    end
 
-    def self.search_node
-      ns = {"xmlns:soap" => "definition"}
+      def self.search_node
+        ns = {"xmlns:soap" => "definition"}
 
-      search = Nokogiri::XML::Builder.new do |xml|
-        xml.root(ns) do
-          xml["soap"].Search07a do
-            xml["soap"].SearchDefinition do
-              xml["soap"].creditrequest({ "schemaversion" => "7.0", "datasets" => "255" }) do
-                xml["soap"].applicant
+        search = Nokogiri::XML::Builder.new do |xml|
+          xml.root(ns) do
+            xml["soap"].Search07a do
+              xml["soap"].SearchDefinition do
+                xml["soap"].creditrequest({ "schemaversion" => "7.0", "datasets" => "255" }) do
+                  xml["soap"].applicant
+                end
               end
             end
           end
         end
+        search.doc.root.children.to_xml
       end
-      search.doc.root.children.to_xml
-    end
 
 
-    def self.purpose
-      ns = {"xmlns:soap" => "definition"}
-      purp = Nokogiri::XML::Builder.new do |xml|
-        xml.root(ns) do
-          xml["soap"].score 1
-          xml["soap"].purpose "TV"
-          xml["soap"].autosearch 1
-          xml["soap"].autosearchmaximum 3
-        end
-      end
-      purp.doc.root.children.to_xml
-    end
-
-    def self.address(number, postcode)
-      ns = {"xmlns:soap" => "definition"}
-      address = Nokogiri::XML::Builder.new do |xml|
-        xml.root(ns) do
-          xml["soap"].address do
-            xml["soap"].buildingno number
-            xml["soap"].postcode postcode
+      def self.purpose
+        ns = {"xmlns:soap" => "definition"}
+        purp = Nokogiri::XML::Builder.new do |xml|
+          xml.root(ns) do
+            xml["soap"].score 1
+            xml["soap"].purpose "TV"
+            xml["soap"].autosearch 1
+            xml["soap"].autosearchmaximum 3
           end
         end
+        purp.doc.root.children.to_xml
       end
-      address.doc.root.children.to_xml
-    end
 
-
-    def self.tenant(forename, surname, dob)
-      ns = {"xmlns:soap" => "definition"}
-      tenant = Nokogiri::XML::Builder.new do |xml|
-        xml.root(ns) do
-          xml["soap"].name do
-            xml["soap"].forename forename
-            xml["soap"].surname surname
-          end
-          xml["soap"].dob dob
-          xml["soap"].hho 0
-          xml["soap"].tpoptout 1
-          xml["soap"].applicantdemographics do
-            xml["soap"].employment
+      def self.address(number, postcode)
+        ns = {"xmlns:soap" => "definition"}
+        address = Nokogiri::XML::Builder.new do |xml|
+          xml.root(ns) do
+            xml["soap"].address do
+              xml["soap"].buildingno number
+              xml["soap"].postcode postcode
+            end
           end
         end
+        address.doc.root.children.to_xml
       end
-      tenant.doc.root.children.to_xml
-    end
+
+
+      def self.tenant(forename, surname, dob)
+        ns = {"xmlns:soap" => "definition"}
+        tenant = Nokogiri::XML::Builder.new do |xml|
+          xml.root(ns) do
+            xml["soap"].name do
+              xml["soap"].forename forename
+              xml["soap"].surname surname
+            end
+            xml["soap"].dob dob
+            xml["soap"].hho 0
+            xml["soap"].tpoptout 1
+            xml["soap"].applicantdemographics do
+              xml["soap"].employment
+            end
+          end
+        end
+        tenant.doc.root.children.to_xml
+      end
 
   end
 end
