8d609b440499a33da2732318228147fbee866ef3
Alex <alexsiri7@gmail.com>
2014-06-04 15:47:42 +0100

    Added more information in error messages

--- lib/callcredit/search.rb
+++ lib/callcredit/search.rb
@@ -21,7 +21,10 @@ module CallCredit
       @response = @client.call(:search07a, xml: payload)
       CallCredit::JSONmaker.parse @response.to_xml
       rescue Savon::Error => error
-        raise CallCredit::DataError, error.to_hash[:fault][:faultstring]
+        error_code = error.to_hash[:fault][:faultcode]
+        error_string = error.to_hash[:fault][:faultstring]
+        error_detail = error.to_hash[:fault][:detail][:error_block][:error]
+        raise CallCredit::DataError, "#{error_code}: #{error_string} ---- #{error_detail}"
     end
 
     def add_address(*args)
