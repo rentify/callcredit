d0abf765e62a749628d5a11b12bcd278745637ae
Alex <alexsiri7@gmail.com>
2014-06-05 15:22:08 +0100

    Allow abodeno as parameter for address search

--- lib/callcredit/search.rb
+++ lib/callcredit/search.rb
@@ -64,7 +64,7 @@ module CallCredit
     end
 
     def validate_address_params(*args)
-      valid_attribs = [:number, :buildingname, :postcode, :street, :place]
+      valid_attribs = [:abodeno, :number, :buildingname, :postcode, :street, :place]
 
       args.flatten.each do |arg|
         arg.each do |k,v|
